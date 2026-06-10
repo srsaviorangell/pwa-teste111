29e2611ae8ea9a4779edbb68c2f11ae34d3962db
srsaviorangell <sr188bet@gmail.com>
2026-06-10 14:29:52 -0300

    savio louco

--- app/pages/maps/maps.tsx
+++ app/pages/maps/maps.tsx
@@ -4,13 +4,11 @@ import  MapaGeral  from '../../../src/mapaComponent/mapsGeral';
 
 export default function Maps() {
   return (
-    <ScrollView style={{ flex: 1 }} className="h-full w-full items-center justify-center">
-      <View className=" h-full w-full items-center justify-center">
    
       <MapaGeral />
+      
+      
     
-      </View>
-    </ScrollView>
   )
 }
 
--- src/mapaComponent/mapsGeral.tsx
+++ src/mapaComponent/mapsGeral.tsx
@@ -1,39 +1,19 @@
 import { useState } from 'react';
-import { View, Image, Pressable, Text, ScrollView } from 'react-native';
-import TotalSvg from '../../assets/images/total.svg';
+import { View, Image, Pressable, ScrollView, Text } from 'react-native';
 
 export default function MapaGeral() {
   const [zoom, setZoom] = useState(1);
 
   return (
-    <View >
-      <View className='border top-[]'>
-        <Pressable
-          onPress={() => setZoom(z => Math.max(0.5, z - 0.2))}
-        >
-          <Text style={{ fontSize: 24 }}>➖</Text>
-        </Pressable>
-
-        <Pressable
-          onPress={() => setZoom(z => Math.min(5, z + 0.2))}
-        >
-          <Text style={{ fontSize: 24 }}>➕</Text>
-        </Pressable>
-
-     </View>
-
-      <ScrollView
-        horizontal
-        maximumZoomScale={5}
-        minimumZoomScale={1}
-      >
-        <ScrollView>
-          <TotalSvg
-            width={400}
-            height={485}
-          />
-        </ScrollView>
-      </ScrollView>
-    </View>
+    <>
+      <View
+  style={{
+    width: 100,
+    height: 100,
+    backgroundColor: "red",
+  }}
+/>
+      
+    </>
   );
 }
