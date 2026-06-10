fca3abcdebd034c071709067ff19eef053c17c28
savio rangel souza costa <sr188bet@gmail.com>
2026-06-09 22:37:54 -0300

    casa terca id 01

--- app/pages/maps/maps.tsx
+++ app/pages/maps/maps.tsx
@@ -4,11 +4,11 @@ import  MapaGeral  from '../../../src/mapaComponent/mapsGeral';
 
 export default function Maps() {
   return (
-    <ScrollView style={{ flex: 1 }} className="flex-1 items-center justify-center">
-      <View className="border h-full w-full items-center justify-center">
+    <ScrollView style={{ flex: 1 }} className="h-full w-full items-center justify-center">
+      <View className=" h-full w-full items-center justify-center">
    
       <MapaGeral />
-      
+    
       </View>
     </ScrollView>
   )
