c5ad0ba4386b417044676997ef5c7d881c8ccd20
srsaviorangell <sr188bet@gmail.com>
2026-06-13 00:26:00 -0300

    favoritos implementados 2

--- src/conext/EventContext.tsx
+++ src/conext/EventContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import type { Evento } from '../requisicao/listaProgamacao';
 
 interface EventContextType {
@@ -11,6 +12,12 @@ export const EventContext = createContext({} as EventContextType);
 
 export function EventProvider({ children}: any){
     const [eventos, setEventos] = useState<Evento[]>([]);
+
+    useEffect(() => {
+      AsyncStorage.getItem("eventos_completos").then((data) => {
+        if (data) setEventos(JSON.parse(data));
+      });
+    }, []);
 
     return (
         <EventContext.Provider value={{ eventos, setEventos }}>
